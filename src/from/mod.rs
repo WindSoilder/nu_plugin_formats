59713779730faeb8c5b00381bc1d738831a81aa7
WindSoilder <WindSoilder@outlook.com>
2023-02-03 14:29:45 +0800

    add from ics, and re-org test code

--- src/from/mod.rs
+++ src/from/mod.rs
@@ -1,1 +1,2 @@
 pub mod eml;
+pub mod ics;
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 mod from;
-use from::eml;
+
+use from::{eml, ics};
 use nu_plugin::{EvaluatedCall, LabeledError, Plugin};
 use nu_protocol::{Category, Signature, SyntaxShape, Type, Value};
 
@@ -7,15 +8,20 @@ pub struct FromCmds;
 
 impl Plugin for FromCmds {
     fn signature(&self) -> Vec<Signature> {
-        vec![Signature::build(eml::CMD_NAME)
-            .input_output_types(vec![(Type::String, Type::Record(vec![]))])
-            .named(
-                "preview-body",
-                SyntaxShape::Int,
-                "How many bytes of the body to preview",
-                Some('b'),
-            )
-            .category(Category::Formats)]
+        vec![
+            Signature::build(eml::CMD_NAME)
+                .input_output_types(vec![(Type::String, Type::Record(vec![]))])
+                .named(
+                    "preview-body",
+                    SyntaxShape::Int,
+                    "How many bytes of the body to preview",
+                    Some('b'),
+                )
+                .category(Category::Formats),
+            Signature::build(ics::CMD_NAME)
+                .input_output_types(vec![(Type::String, Type::Table(vec![]))])
+                .category(Category::Formats),
+        ]
     }
 
     fn run(
@@ -26,6 +32,7 @@ impl Plugin for FromCmds {
     ) -> Result<Value, LabeledError> {
         match name {
             eml::CMD_NAME => eml::from_eml_call(call, input),
+            ics::CMD_NAME => ics::from_ics_call(call, input),
             _ => Err(LabeledError {
                 label: "Plugin call with wrong name signature".into(),
                 msg: "the signature used to call the plugin does not match any name in the plugin signature vector".into(),
